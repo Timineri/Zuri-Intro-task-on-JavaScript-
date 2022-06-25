3cf72490bf213a93c2c5efa0daf44bf02e4c144c
Timineri Okoko <71822355+Timineri@users.noreply.github.com>
2022-06-25 16:15:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var name = "OKOKO TIMINERI PRINCESS"
+var height = "4.5ft"
+var country = "NIGERIA"
+
+document.write("Full name : " + name);
+document.write("<br>");
+document.write("<br>");
+
+document.write("Height : " + height);
+document.write("<br>");
+document.write("<br>");
+
+document.write("Country : " + country);
